a53930a8ee928f0608eff0b4b183cefcb689cc91
dvinubius <dvinubiu.narmon@gmail.com>
2024-06-11 23:45:39 +0300

    cleanup

--- test/process_test.lua
+++ test/process_test.lua
@@ -2,7 +2,7 @@
 require("test.setup")()
 
 _G.VerboseTests = true
--- _G.VirtualTime = os.time() -- optional, uncomment if you need time travel in tests
+_G.VirtualTime = _G.VirtualTime or nil -- use for time travel
 
 _G.Owner = '123MyOwner321'
 _G.MainProcessId = '123xyzMySelfabc321'
